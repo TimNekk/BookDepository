24dc4071acf4214713d1ae0a71dc929d285ae62a
Tim <herew26@gmail.com>
2023-02-10 11:35:17 +0300

    feat: init new storages

--- src/main/java/timnekk/Main.java
+++ src/main/java/timnekk/Main.java
@@ -2,13 +2,17 @@ package timnekk;
 
 import timnekk.input.ConsoleCommandProviderFactory;
 import timnekk.output.ConsoleOutputWriter;
-import timnekk.storage.JsonBookStorage;
+import timnekk.storage.MemoryBookBundle;
+import timnekk.storage.JsonBookLibrary;
+
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
         ApplicationSettings settings = new ApplicationSettings(
                 new ConsoleCommandProviderFactory(),
-                new JsonBookStorage(),
+                new JsonBookLibrary(new File("src/main/resources/books.json").toPath()),
+                new MemoryBookBundle(),
                 new ConsoleOutputWriter()
         );
 
