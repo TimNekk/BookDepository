b06749783a91335afebda0bcd545f1967e2bbf55
Tim <herew26@gmail.com>
2023-02-10 11:33:44 +0300

    feat: add exit command

---
+++ src/main/java/timnekk/commands/ExitCommand.java
@@ -0,0 +1,29 @@
+package timnekk.commands;
+
+import timnekk.exceptions.CommandExecutionException;
+import timnekk.exceptions.StorageException;
+import timnekk.storage.BookStorage;
+import timnekk.models.Book;
+
+public class ExitCommand extends Command {
+    private final BookStorage bundle;
+    private final BookStorage library;
+
+    public ExitCommand(BookStorage bundle, BookStorage library) {
+        this.bundle = bundle;
+        this.library = library;
+    }
+
+    @Override
+    public void execute() throws CommandExecutionException {
+        try {
+            for (Book book : bundle.getBooks()) {
+                library.addBook(book);
+            }
+        } catch (StorageException e) {
+            throw new CommandExecutionException(e);
+        }
+
+        System.exit(0);
+    }
+}
